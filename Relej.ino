672e02c624854519a0f644580da666a7978ef877
simunsauka <simun.sauka@gmail.com>
2026-09-17 00:37:08 +0200

    Update Relej.ino

--- Relej.ino
+++ Relej.ino
@@ -1,22 +1,20 @@
-/*
 
-*/
 
 #define REL 15
 
 
-// the setup function runs once when you press reset or power the board
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  
   pinMode(REL, OUTPUT);
 }
 
-// the loop function runs over and over again forever
+
 void loop() { 
 
-  digitalWrite(REL, LOW);   // turn the LED off by making the voltage LOW
-  delay(30000);                      // wait for a second
-  digitalWrite(REL, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(2000);                      // wait for a second
+  digitalWrite(REL, LOW);   
+  delay(30000);                      
+  digitalWrite(REL, HIGH);  
+  delay(2000);                      
 
 }
